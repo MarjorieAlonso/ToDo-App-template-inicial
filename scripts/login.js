6118f052d528f3d43e0c18e176c145634632109e
MarjorieAlonso <matirodraven@gmail.com>
2024-03-19 23:03:09 -0300

    primer commit

--- scripts/login.js
+++ scripts/login.js
@@ -1,19 +1,34 @@
 window.addEventListener('load', function () {
     /* ---------------------- obtenemos variables globales ---------------------- */
-   
-    
 
 
+    const form = document.forms[0];
+
+    const email = document.querySelector("#inputEmail")
+    const password = document.querySelector("#inputPassword")
+    const url = "https://todo-api.ctd.academy/v1"
 
     /* -------------------------------------------------------------------------- */
     /*            FUNCIÓN 1: Escuchamos el submit y preparamos el envío           */
     /* -------------------------------------------------------------------------- */
     form.addEventListener('submit', function (event) {
-       
-        
 
+        event.preventDefault();
+        const payload = {
+            email: email.value,
+            password: password.value
+        }
 
+        const settings = {
+            method: "POST",
+            body: JSON.stringify(payload),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+        realizarLogin(settings)
 
+        form.reset();
     });
 
 
@@ -21,12 +36,36 @@ window.addEventListener('load', function () {
     /*                     FUNCIÓN 2: Realizar el login [POST]                    */
     /* -------------------------------------------------------------------------- */
     function realizarLogin(settings) {
-       
+        fetch(`${url}/users/login`, settings)
+            .then(response => {
+                if (response.ok)
+                    return response.json()
+                return Promise.reject(response)
+            })
+            .then(data => {
+                if (data.jwt) {
+                    localStorage.setItem("jwt", JSON.stringify(data.jwt))
+
+                    form.reset();
+
+                    location.replace("./mis-tareas.html")
+                }
+
+            })
+            .catch(err => {
+                if (err.status === 400) {
+                    alert("contraseña incorrecta")
+                } else if (err.status === 404) {
+                    alert("usuario no encontrado")
+                } else {
+                    alert("error del servidor")
+                }
+                console.error(err.status)
+            })
 
 
 
 
-        
     };
 
 
--- scripts/signup.js
+++ scripts/signup.js
@@ -1,18 +1,40 @@
 window.addEventListener('load', function () {
     /* ---------------------- obtenemos variables globales ---------------------- */
-   
+    //const form = document.querySelector("#form-header");
+    const form = document.forms[0];
+    const nombre = document.querySelector("#inputNombre")
+    const apellido = document.querySelector("#inputApellido");
+    const mail = document.querySelector("#inputEmail")
+    const contrasenia = document.querySelector("#inputPassword")
+    const repetir = document.querySelector("#inputPasswordRepetida")
+    const url = "https://todo-api.ctd.academy/v1"
 
 
-    
 
     /* -------------------------------------------------------------------------- */
     /*            FUNCIÓN 1: Escuchamos el submit y preparamos el envío           */
     /* -------------------------------------------------------------------------- */
     form.addEventListener('submit', function (event) {
-      
+        event.preventDefault();
 
+        const peyloud = {
 
+            firstName: nombre.value,
+            lastName: apellido.value,
+            email: mail.value,
+            password: contrasenia.value
 
+        }
+        const settings = {
+            method: "POST",
+            body: JSON.stringify(peyloud),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+
+        realizarRegister(settings)
+        form.reset()
 
     });
 
@@ -20,12 +42,34 @@ window.addEventListener('load', function () {
     /*                    FUNCIÓN 2: Realizar el signup [POST]                    */
     /* -------------------------------------------------------------------------- */
     function realizarRegister(settings) {
-        
+        fetch(`${url}/users`, settings)
+            .then(response => {
+                if (response.ok) {
+                    return response.json()
+                }
+                return Promise.reject(response)
+            })
+            .then(data => {
+                if (data.jwt) {
+                    localStorage.setItem("jwt", JSON.stringify(data.jwt))
+                    form.reset();
+                    location.replace("./index.html")
+                }
+
+            })
+            .catch(err => {
+                if (err.status === 400) {
+                    alert("El usuario ya se encuentra registrado ")
+                } else if (err.status === 401) {
+                    alert("Alguno de los datos requeridos está incompleto")
+
+                } else {
+                    alert("Error del servidor")
+                }
+            })
+    }
 
 
 
 
-    };
-
-
-});
+})
